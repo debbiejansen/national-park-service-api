9be7057c4f1e87b6e1d005af10d136d60bb8c940
debbiejansen <debbiejansen94@hotmail.com>
2025-12-05 16:41:25 +0100

    start header design

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,16 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <nav>
+            <ul>
+                <li><a href={"/"}>About</a></li>
+                <li><a href={"/"}>Blog</a></li>
+            </ul>
+        </nav>
+        <header>
+            <h1>National Park Service</h1>
+            <button type="button" onClick={logClick}>Login</button>
+        </header>
     </>
   )
 }
