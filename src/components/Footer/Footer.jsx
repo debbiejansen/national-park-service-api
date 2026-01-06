4dbfbeb0d0a893a1f9eae29b76e0ff9239741094
debbiejansen <debbiejansen94@hotmail.com>
2026-01-06 11:41:11 +0100

    footer elements toegevoegd

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,18 +1,37 @@
 import './Footer.css'
 import React from 'react';
 import logo from '../../assets/National-Park-Service-Logo-1968.png';
+import {NavLink} from 'react-router-dom';
 
 function Footer() {
     return (
 
         <footer className="outer-container-footer">
-            <ul>
+            <div>
+                <img src={logo}
+                     alt="Company logo"
+                     className="footer-logo"/>
+            </div>
+
+            <ul className="footer-links">
                 <li>
-                    {logo}
+                <h3>Quick Acces</h3>
+                </li>
+                <li>
+                    <NavLink
+                        className={({isActive}) => isActive ? 'active-menu-link' : 'default-menu-link'}
+                        to="/about">
+                        About
+                    </NavLink>
                 </li>
 
             </ul>
 
+            <div className="footer-quote">
+                <h3>National Park Service</h3>
+                <p>The national park community welcomes you! Together we can celebrate these special places and ensure
+                    they exist forever.</p>
+            </div>
         </footer>
 
     );
