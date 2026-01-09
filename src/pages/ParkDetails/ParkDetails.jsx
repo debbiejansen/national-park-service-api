98a8dc12d796c09345e5ceccc4159e5959dd479a
debbiejansen <debbiejansen94@hotmail.com>
2026-01-09 22:21:10 +0100

    paginas aangepast

---
+++ src/pages/ParkDetails/ParkDetails.jsx
@@ -0,0 +1,15 @@
+import './ParkDetails.css'
+import React from 'react';
+
+function ParkDetails() {
+    return (
+
+        <main className="page-container-park-details">
+            <h2>Parkdetails</h2>
+            <p>Take me back to the</p>
+        </main>
+
+    );
+}
+
+export default ParkDetails
