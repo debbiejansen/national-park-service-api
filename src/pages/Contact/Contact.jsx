f9c37e3b77b61a5b84bd0b0b8002c76f675d8a0b
debbiejansen <debbiejansen94@hotmail.com>
2026-01-10 20:03:17 +0100

    eerste basis contact page

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 function Contact() {
     return (
 
-        <main className="page-container-not-found">
+        <main className="page-container-contact">
             <h2>Oops... This page doesn't exist</h2>
             <p>Contactpage </p>
         </main>
