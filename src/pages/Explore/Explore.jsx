41d9c492293c7331bec285323f1c2c3844b40f27
debbiejansen <debbiejansen94@hotmail.com>
2026-03-01 18:32:55 +0100

    explore basis

--- src/pages/Explore/Explore.jsx
+++ src/pages/Explore/Explore.jsx
@@ -5,8 +5,10 @@ function Explore() {
     return (
         <>
             <div className="outer-container">
+                <div className="page-container-explore">
             <h1>Test Explore page</h1>
             <p>test test</p>
+                </div>
             </div>
         </>
     );
