af01f0ca5f3d718189de30b1684758ec4c573ef5
debbiejansen <debbiejansen94@hotmail.com>
2026-03-01 14:42:36 +0100

    about page opgemaakt

--- src/pages/ParkDetails/ParkDetails.jsx
+++ src/pages/ParkDetails/ParkDetails.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 
 function ParkDetails() {
     return (
-
+<div className="outer-container">
         <main className="page-container-park-details">
             <h2>Parkdetails</h2>
             <p>Take me back to the</p>
         </main>
-
+</div>
     );
 }
 
