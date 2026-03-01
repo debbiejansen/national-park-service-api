c8073e0ab47b4cfd694b3d43dd1a5ff51bf79c5c
debbiejansen <debbiejansen94@hotmail.com>
2026-03-01 19:10:24 +0100

    exporetile gemaakt, basis opgezet

--- src/pages/Explore/Explore.jsx
+++ src/pages/Explore/Explore.jsx
@@ -1,13 +1,38 @@
 import './Explore.css'
 import React from 'react';
+import parkimg from '../../assets/matthew-smith-Rfflri94rs8-unsplash.jpg';
+import ExploreTile from '../../components/ExploreTile/ExploreTile.jsx';
 
 function Explore() {
     return (
         <>
             <div className="outer-container">
+                <div>
+                    Hier komt de filter balk
+                </div>
                 <div className="page-container-explore">
-            <h1>Test Explore page</h1>
-            <p>test test</p>
+
+                    <ExploreTile
+                        title="Andere naam van een park"
+                        discription="Info over park om te testen of dit werkt"
+                        image={parkimg}
+                        to="/about"
+                    />
+
+                    <ExploreTile
+                        title="Andere naam van een park 2"
+                        discription="Info over park om te testen of dit werkt 2"
+                        image={parkimg}
+                        to="/about"
+                    />
+
+                    <ExploreTile
+                        title="Andere naam van een park 3"
+                        discription="Info over park om te testen of dit werkt 3"
+                        image={parkimg}
+                        to="/about"
+                    />
+
                 </div>
             </div>
         </>
