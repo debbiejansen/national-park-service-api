020125ca79769024655ef1acdc6559f2dcce036c
debbiejansen <debbiejansen94@hotmail.com>
2026-03-07 17:51:57 +0100

    button kleur variant aangemaakt

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,12 +1,15 @@
 import './Button.css'
 
-function Button({ label, type, disabled, onClick }) {
+function Button({ label, type, disabled, onClick, variant }) {
+
+    const variantClass = variant === 'white' ? 'btn-white' : 'btn-green';
 
     return (
         <button
             type={type}
             disabled={disabled}
             onClick={onClick}
+            className={`btn ${variantClass}`}
         >
             {label}
         </button>
